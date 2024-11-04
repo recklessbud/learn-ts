13e86e9bb70b8e271c047487cd26d6e6c526e262
Alexander <alexander478@gmail.com>
2024-11-04 15:44:09 +0000

    npm i typescript

---
+++ day-1/hello.js
@@ -0,0 +1,5 @@
+function personS(name, date) {
+    console.log("Hello, ".concat(name, "!"));
+    console.log("Happy ".concat(date, "!"));
+}
+personS('World', new Date());
---
+++ day-1/hello.ts
@@ -0,0 +1,7 @@
+ function personS(name: string, date: Date) {
+    console.log(`Hello, ${name}!`);
+    console.log(`Happy ${date}!`);
+    
+}
+
+personS('World', new Date());
